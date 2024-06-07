9ab3df8275c85f7c1639fb99f66926ed29d533fa
Nikolas <nikolas@nkpc.local>
2024-06-07 22:14:59 +0200

    some additions

--- src/wiprojss24gr7/userhandling/Document.java
+++ src/wiprojss24gr7/userhandling/Document.java
@@ -1,5 +1,56 @@
 package wiprojss24gr7.userhandling;
 
-public class Document {
+import java.sql.Blob;
+import java.sql.Timestamp;
 
+public class Document {
+	private int dokumentId;
+	private Blob document;
+	private int MNr;
+	private String documentType;
+	private String dateiTyp;
+	private Timestamp zeitStempel;
+	
+	public Document() {
+    }
+
+    public Document(int dokumentId, Blob document, int MNr, String documentType, String dateiTyp, Timestamp zeitStempel) {
+        this.dokumentId = dokumentId;
+        this.document = document;
+        this.MNr = MNr;
+        this.documentType = documentType;
+        this.dateiTyp = dateiTyp;
+        this.zeitStempel = zeitStempel;
+    }
+
+	public int getDokumentId() {
+		return dokumentId;
+	}
+
+
+	public Blob getDocument() {
+		return document;
+	}
+
+	public int getMNr() {
+		return MNr;
+	}
+
+	public String getDocumentType() {
+		return documentType;
+	}
+
+	public String getDateiTyp() {
+		return dateiTyp;
+	}
+
+	public Timestamp getZeitStempel() {
+		return zeitStempel;
+	}
+
+	public void setZeitStempel(Timestamp zeitStempel) {
+		this.zeitStempel = zeitStempel;
+	}
+    
+	
 }
